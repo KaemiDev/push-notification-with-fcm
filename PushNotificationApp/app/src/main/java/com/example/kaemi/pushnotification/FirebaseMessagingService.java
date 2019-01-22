58ebbece01a81ceac38224cfd924e46e7f44441b
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-01-22 12:35:57 -0200

    Fixing bugs on push App

---
+++ PushNotificationApp/app/src/main/java/com/example/kaemi/pushnotification/FirebaseMessagingService.java
@@ -0,0 +1,101 @@
+package com.example.kaemi.pushnotification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        FirebaseInstanceId.getInstance().getInstanceId()
+            .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                @Override
+                public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()) {
+                        //To do//
+                        return;
+                    }
+
+                    // Get the Instance ID token//
+                    String token = task.getResult().getToken();
+                    Log.d("Token: ", token);
+
+                    // Save the token on device
+                    SharedPreferences mSettings = getApplicationContext().getSharedPreferences("Settings", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = mSettings.edit();
+                    editor.remove("Token");
+                    editor.putString("Token", token);
+                    editor.apply();
+
+                }
+            });
+
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Map<String, String> params = remoteMessage.getData();
+        JSONObject object = new JSONObject(params);
+        Log.e("JSON_OBJECT", object.toString());
+
+        String NOTIFICATION_CHANNEL_ID = "Kaemi Channel";
+
+        long pattern[] = {0, 1000, 500, 1000};
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Your Notifications",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(pattern);
+            notificationChannel.enableVibration(true);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        // to diaplay notification in DND Mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = mNotificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID);
+            channel.canBypassDnd();
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+
+}
