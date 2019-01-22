b23f84a059f9c9f89ec362e25ac844ad033ab433
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-01-22 12:41:19 -0200

    addNotificationMessage Function

--- Cloud Functions/functions/index.js
+++ Cloud Functions/functions/index.js
@@ -1,8 +1,23 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addNotificationMessage = functions.https.onRequest((req, res) => {
+
+    const title = req.query.title;
+    const body = req.query.body;
+
+    console.log("Title", title, "Body:", body);
+
+    var refNotification = admin.database().ref('/Notification/Message');
+    refNotification.set
+    ({
+        "Body": body,
+        "Title": title
+    });
+
+    res.status(200).send("OK");
+});
